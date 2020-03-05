e4c71a97a652a490cc2e0c7afb97390e63b8aee3
Jack Gribben <jack.gribben@cbg.co.nz>
2020-03-05 15:31:55 +1300

    Formatting changes as requested by Neil

--- LeicaLaserInterface/MainWindow.xaml.cs
+++ LeicaLaserInterface/MainWindow.xaml.cs
@@ -177,9 +177,16 @@ namespace LeicaLaserInterface
             }
         }
 
-            public void updateConnectionStatus(string text)
+        public void updateConnectionStatus(string text)
         {
-            Application.Current.Dispatcher.Invoke(() => { Connectionstatus.Text = text; });
+            if (text == "CONNECTED")
+            {
+                Application.Current.Dispatcher.Invoke(() => { Connectionstatus.Text = text; Connectionstatus.Foreground = Brushes.Green; });
+            }
+            if (text == "Disconnected")
+            {
+                Application.Current.Dispatcher.Invoke(() => { Connectionstatus.Text = text; Connectionstatus.Foreground = Brushes.Black; });
+            }
         }
 
         public void updateH1Text(string text)
